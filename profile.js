f69b2ffd38f41a136fe348b25965bc6c2710059e
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-01-09 14:27:06 +0100

    Add a profile object

---
+++ profile.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const profile = {
+  name: 'Yusuf Oyinlola',
+  nickname: 'YuzStack',
+  profession: 'Frontend Developer/Engineer',
+  tools: ['React', 'JavaScript', 'Tailwind', 'Git/GitHub', 'HTML', 'CSS'],
+};
